45a061de5f560b13dceb3cb7f43d604ece57cf98
ergincetin-at-058264215011 <ergincetn01@gmail.com>
2025-03-08 00:24:29 +0300

    Signin and root components updated

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -3,11 +3,25 @@ import { Text, View } from "react-native"
 
 export default function Index() {
 	return (
-		<View className="items-center justify-center">
-			<Text className="font-bold text-lg"> ddsgdgsd</Text>
-			<Link href="/sign-in">sign in</Link>
-			<Link href="/profile">sign in</Link>
-			<Link href="/explore">explore</Link>
+		<View className="items-center flex-1 bg-primary-100 flex-col">
+			<Link
+				href="/sign-in"
+				className="font-semibold text-xl text-black-300"
+			>
+				Sign in
+			</Link>
+			<Link
+				href="/profile"
+				className="font-semibold text-xl text-black-300"
+			>
+				Profile
+			</Link>
+			<Link
+				href="/explore"
+				className="font-semibold text-xl text-black-300"
+			>
+				Explore
+			</Link>
 		</View>
 	)
 }
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,10 +1,32 @@
-import { View, Text } from "react-native"
+import { View, Text, ScrollView, Image } from "react-native"
 import React from "react"
+import { SafeAreaView } from "react-native-safe-area-context"
+import images from "@/constants/images"
 
 const SignIn = () => {
 	return (
-		<View>
-			<Text>sign-in</Text>
+		<View
+			// style={{ backgroundColor: "white" }
+			className="bg-white h-full"
+		>
+			<ScrollView contentContainerClassName="h-full">
+				<Image
+					source={images.onboarding}
+					className="w-full h-4/6"
+					resizeMode="contain"
+				/>
+				<View className="px-10">
+					<Text className="text-base text-center uppercase font-rubik text-black-200">
+						Welcome to ReState
+					</Text>
+					<Text className="text-3xl text-center mt-2 font-rubik-bold text-black-300">
+						Let's Get You Closer To{"\n"}
+						<Text className="text-primary-300">
+							Your Ideal Home
+						</Text>
+					</Text>
+				</View>
+			</ScrollView>
 		</View>
 	)
 }
