2afc2a8f2ca50c5135ca5a3919b9b70daf19cfea
ergincetin-at-058264215011 <ergincetn01@gmail.com>
2025-03-08 14:17:38 +0300

    new texts and google auth button added to signin

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,9 +1,11 @@
-import { View, Text, ScrollView, Image } from "react-native"
+import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native"
 import React from "react"
 import { SafeAreaView } from "react-native-safe-area-context"
 import images from "@/constants/images"
+import icons from "@/constants/icons"
 
 const SignIn = () => {
+	const handleLogin = () => {}
 	return (
 		<View
 			// style={{ backgroundColor: "white" }
@@ -25,6 +27,24 @@ const SignIn = () => {
 							Your Ideal Home
 						</Text>
 					</Text>
+					<Text className="text-lg text-black-200 font-rubik text-center mt-10">
+						Login to ReState with Google
+					</Text>
+					<TouchableOpacity
+						onPress={handleLogin}
+						className="rounded-full shadow-md shadow-zinc-300 bg-white w-full py-4 mt-5"
+					>
+						<View className="items-center flex flex-row justify-center">
+							<Image
+								source={icons.google}
+								className="w-5 h-5"
+								resizeMode="contain"
+							/>
+							<Text className="font-semibold text-black-300 ml-2">
+								Continue with Google
+							</Text>
+						</View>
+					</TouchableOpacity>
 				</View>
 			</ScrollView>
 		</View>
