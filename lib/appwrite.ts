32612304c1ecd0f4866705c52ce131ad44e894d9
ergincetin-at-058264215011 <ergincetn01@gmail.com>
2025-03-08 14:17:19 +0300

    appwrite and google auth settings handled

---
+++ lib/appwrite.ts
@@ -0,0 +1,63 @@
+import { Account, Avatars, Client, OAuthProvider } from "react-native-appwrite"
+import * as Linking from "expo-linking"
+import { openAuthSessionAsync } from "expo-web-browser"
+export const config = {
+	platform: "com.restate",
+	endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+	projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+}
+
+export const client = new Client()
+
+client
+	.setEndpoint(config.endpoint!)
+	.setProject(config.projectId!)
+	.setPlatform(config.platform!)
+
+export const avatar = new Avatars(client)
+export const account = new Account(client)
+
+export async function login() {
+	try {
+		const redirectUri = Linking.createURL("./")
+		const res = await account.createOAuth2Token(
+			OAuthProvider.Google,
+			redirectUri
+		)
+		if (!res) throw new Error("Failed to login")
+
+		const browserResponse = await openAuthSessionAsync(
+			res.toString(),
+			redirectUri
+		)
+
+		if (browserResponse.type !== "success")
+			throw new Error("Failed to login!")
+
+		const url = new URL(browserResponse.url)
+
+		const secret = url.searchParams.get("secret")?.toString()
+		const userId = url.searchParams.get("userId")?.toString()
+
+		if (!secret || !userId) throw new Error("Failed to login!")
+
+		const session = await account.createSession(userId, secret)
+
+		if (!session) throw new Error("Failed to login!")
+
+		return true
+	} catch (error) {
+		console.error(error)
+		return false
+	}
+}
+
+export async function logout() {
+	try {
+		await account.deleteSession("current")
+		return true
+	} catch (err) {
+		console.error(err)
+		return false
+	}
+}
