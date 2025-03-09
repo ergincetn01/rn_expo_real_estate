745825d52d8c01284702273c767555de80629529
ergincetin-at-058264215011 <ergincetn01@gmail.com>
2025-03-10 00:01:41 +0300

    globalprovider implemented

---
+++ lib/global-provider.tsx
@@ -0,0 +1,46 @@
+import React, { createContext, useContext } from "react"
+import { useAppwrite } from "./useAppwrite"
+import { getCurrentUser } from "./appwrite"
+
+interface User {
+	$id: string
+	name: string
+	email: string
+	avatar: string
+}
+interface GlobalContextType {
+	isLoggedIn: boolean
+	user: User | null
+	loading: boolean
+	refetch: (newParams?: Record<string, string | number>) => Promise<void>
+}
+const GlobalContext = createContext<GlobalContextType | undefined>(undefined)
+
+export const GlobalProvider = ({ children }: { children: React.ReactNode }) => {
+	const {
+		data: user,
+		loading,
+		refetch,
+	} = useAppwrite({
+		fn: getCurrentUser,
+	})
+
+	const isLoggedIn = !!user
+	return (
+		<GlobalContext.Provider value={{ loading, refetch, user, isLoggedIn }}>
+			{children}
+		</GlobalContext.Provider>
+	)
+}
+
+export const useGlobalProvider = (): GlobalContextType => {
+	const context = useContext(GlobalContext)
+
+	if (!context) {
+		throw new Error("useGlobalContext must be used within GlobalProvider")
+	}
+
+	return context
+}
+
+export default GlobalProvider
